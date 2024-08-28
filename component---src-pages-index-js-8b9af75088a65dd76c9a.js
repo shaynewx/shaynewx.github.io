db46c13d73e95f147bbb07d3b615a991fbbfe19b
Shayne Wang <shayne.wx1997@gmail.com>
2024-08-29 00:39:51 +0800

    Updates

---
+++ component---src-pages-index-js-8b9af75088a65dd76c9a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshayne_s_gatsby_site=self.webpackChunkshayne_s_gatsby_site||[]).push([[293],{9639:function(e,t,n){n.r(t),n.d(t,{Head:function(){return s}});var a=n(6540),l=n(4810);const s=()=>a.createElement("title",null,"Home Page");t.default=()=>a.createElement("main",null,a.createElement("h1",null,"Welcome to my Gatsby site!"),a.createElement(l.N_,{to:"/about"},"About"),a.createElement("p",null,"I'm making this by following the Gatsby Tutorial."))}}]);
+//# sourceMappingURL=component---src-pages-index-js-8b9af75088a65dd76c9a.js.map
