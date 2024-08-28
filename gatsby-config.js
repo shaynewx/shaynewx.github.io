4f11ccd9377acb9d63876ae5203cd221e345be30
Shayne Wang <shayne.wx1997@gmail.com>
2024-08-28 22:13:50 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Shayne's Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
